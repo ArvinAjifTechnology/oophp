928f9b19de93d942ab805e4388bc42db7b352f8e
ArvinAjifTechnology <arvinajif3@gmail.com>
2022-02-06 01:36:12 +0700

    Mngedit Produk 3

--- produk.php
+++ produk.php
@@ -11,10 +11,16 @@ class Produk
         $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new Produk();
-$produk2->judul = "uncharted";
-var_dump($produk2);
+// $produk2 = new Produk();
+// $produk2->judul = "uncharted";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$Produk3->penulis = "Masashi Kisimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
