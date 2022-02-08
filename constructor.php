bc49e8d5cba26d7ae597f5fd3ae7c21550e51a55
ArvinAjifTechnology <arvinajif3@gmail.com>
2022-02-08 09:53:33 +0700

    Mengedit File Constuctor

--- constructor.php
+++ constructor.php
@@ -24,7 +24,7 @@ class Produk
 
 
 $produk1 = new Produk("Naruto", "Masashi Kisimoto", "Shonen Jump", 30000);
-$produk4 = new Produk("Uncharted", "Neil Drucmann", "Sony Computer", 250000);
+$produk2 = new Produk("Uncharted", "Neil Drucmann", "Sony Computer", 250000);
 
 echo "Komik : " . $produk1->getLabel();
 echo "<br>";
